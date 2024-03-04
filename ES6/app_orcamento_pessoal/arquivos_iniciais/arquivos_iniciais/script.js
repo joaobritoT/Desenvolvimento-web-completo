b76f5cb13e945cbe2c28cdca4d4d64008d6a7690
Joao Victor <joaobrito265@outlook.com>
2024-03-04 08:03:42 -0300

    04/03/2024

---
+++ ES6/app_orcamento_pessoal/arquivos_iniciais/arquivos_iniciais/script.js
@@ -0,0 +1,47 @@
+class Despesa{
+    constructor(ano,mes,dia,tipo,descricao,valor){
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao 
+        this.valor = valor
+    }
+}
+
+class Bd{
+    constructor(){
+        let id = localStorage.getItem('id')
+
+        if(id === null){
+            localStorage.setItem('id',0)
+        }
+    }
+    getProximoId(){
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1
+    }
+
+    gravar(d){
+        let id = this.getProximoId()
+        localStorage.setItem(id,JSON.stringify(d))
+        localStorage.setItem('id',id)
+    }
+}
+
+let bd = new Bd
+
+function cadastrarDespesa(){
+    let ano = document.getElementById("ano").value
+    let mes = document.getElementById("mes").value
+    let dia = document.getElementById("dia").value
+    let tipo = document.getElementById("tipo").value
+    let descricao = document.getElementById("descricao").value
+    let valor = document.getElementById("valor").value
+
+    let despesa = new Despesa(ano,mes,dia,tipo,descricao,valor)
+    console.log(despesa)
+    bd.gravar(despesa)
+
+}
+
